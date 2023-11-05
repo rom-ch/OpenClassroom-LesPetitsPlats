3c39bcf82c28b8f4e21ada5efd47718d57ad5293
Romain Chauvin <rom.chauvin@gmail.com>
2023-11-05 22:05:20 +0100

    array branch

--- src/utils/mainSearch.js
+++ src/utils/mainSearch.js
@@ -1,36 +1,24 @@
 import normalizeString from "./normalizeString.js";
 
-export default class MainSearch {
-
+export default class MainSearchArrMethods {
   static filterCards(recipes, userInput) {
-    let filteredRecipes = [];
+    const filteredRecipes = recipes.filter(recipe => {
+      const compareIngredients = recipe.ingredients.some(ing => {
+        const checkIngredient = normalizeString(ing.ingredient).includes(
+          normalizeString(userInput)
+        );
+        if (checkIngredient) return recipe;
+      });
 
-    for (let i = 0; i < recipes.length; i++) {
-      const compareName = normalizeString(recipes[i].name).includes(
-        normalizeString(userInput)
+      return (
+        normalizeString(recipe.name).includes(normalizeString(userInput)) ||
+        normalizeString(recipe.description).includes(
+          normalizeString(userInput)
+        ) ||
+        compareIngredients
       );
-      const compareDescription = normalizeString(
-        recipes[i].description
-      ).includes(normalizeString(userInput));
+    });
 
-      if (compareName) {
-        filteredRecipes.push(recipes[i]);
-        continue;
-      } else if (compareDescription) {
-        filteredRecipes.push(recipes[i]);
-        continue;
-      } else {
-        for (let j = 0; j < recipes[i].ingredients.length; j++) {
-          const compareIngredients = normalizeString(
-            recipes[i].ingredients[j].ingredient
-          ).includes(normalizeString(userInput));
-
-          if (compareIngredients) {
-            filteredRecipes.push(recipes[i]);
-          }
-        }
-      }
-    }
     return filteredRecipes;
   }
 }
